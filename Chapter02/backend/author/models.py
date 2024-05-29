a1b8095fa5fe5efdbeaf4ab1321f2cc10ffdbb7e
khayratdinov <khayratdinov.np@gmail.com>
2024-05-29 15:27:05 +0500

    📚 I finished Chapter 02

---
+++ Chapter02/backend/author/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogAuthor(Author):
+    class Meta:
+        proxy = True
+
+    def perform_something(self):
+        pass
