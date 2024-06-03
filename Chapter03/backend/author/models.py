bad95c03d3d06531f0101f5058585e8ee78f6d5e
khayratdinov <khayratdinov.np@gmail.com>
2024-06-03 15:17:03 +0500

    📚 I finished Chapter 03

--- Chapter03/backend/author/models.py
+++ Chapter03/backend/author/models.py
@@ -3,16 +3,11 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    bio = models.TextField(null=True, blank=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField()
+
+    def fetch_short_bio(self):
+        return self.bio[:100]
 
     def __str__(self):
         return self.name
-
-
-class BlogAuthor(Author):
-    class Meta:
-        proxy = True
-
-    def perform_something(self):
-        pass
