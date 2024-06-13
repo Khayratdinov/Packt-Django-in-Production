71db10633600d1fe63e365776e89c2d97cc9baa2
khayratdinov <khayratdinov.np@gmail.com>
2024-06-13 21:02:43 +0500

    📚 I finished Chapter 04

--- Chapter04/README.md
+++ Chapter04/README.md
@@ -1,11 +1,9 @@
 
-## Serializing Data with DRF
+## Exploring Django Admin and Management Commands
 
 ### We shall cover the following topics in this chapter:
 
-- Understanding the basics of DRF Serializers
-- Using model Serializers
-- Implementing Serializer relations
-- Validating data using Serializers
-- Mastering DRF Serializers
-- Using Serializers with DRF views
+- Exploring Django Admin
+- Customizing Django Admin
+- Optimizing Django Admin for production
+- Creating custom management commands
--- Chapter04/backend/blog/models.py
+++ Chapter04/backend/blog/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
 
+class Blog(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        "author.Author", related_name="author_blogs", on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cover_image = models.OneToOneField(
+        "CoverImage", related_name="blog_cover_image", on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField("Tag", related_name="blog_tags")
+
+    def __str__(self):
+        return self.title
+
+
 class BaseTimeStampModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -9,27 +26,9 @@ class BaseTimeStampModel(models.Model):
         abstract = True
 
 
-class Blog(BaseTimeStampModel):
-    title = models.CharField(max_length=100, unique=True)
-    content = models.TextField()
-    author = models.ForeignKey(
-        "author.Author", related_name="author_blogs", on_delete=models.CASCADE
-    )
-    tags = models.ManyToManyField("Tags", related_name="blog_tags")
-    cover_image = models.OneToOneField(
-        "CoverImage", related_name="blog_cover_image", on_delete=models.PROTECT
-    )
-
-    def __str__(self):
-        return self.title
-
-
 class CoverImage(BaseTimeStampModel):
     image_link = models.URLField()
 
 
-class Tags(BaseTimeStampModel):
+class Tag(BaseTimeStampModel):
     name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
