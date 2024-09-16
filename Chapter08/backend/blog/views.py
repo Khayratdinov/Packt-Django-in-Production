35d10963a86a218f3629d7d29fc5e5336125cf22
khayratdinov <khayratdinov.np@gmail.com>
2024-09-16 16:45:00 +0500

    📚 I finished Chapter 08

--- Chapter08/backend/blog/views.py
+++ Chapter08/backend/blog/views.py
@@ -1,54 +1,23 @@
 from django.http import HttpResponse
-from rest_framework import views
-from rest_framework import response
-from rest_framework import generics
-from rest_framework import status
-from rest_framework import filters
 
+from cacheops import cached
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from blog import models
-from blog import serializers as serializer
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.throttling import AnonRateThrottle
+from rest_framework.throttling import ScopedRateThrottle
 
 
-class BlogGetCreateView(views.APIView):
-    def get(self, request):
-        blogs_obj_list = models.Blog.objects.all()
-        blogs = serializer.BlogSerializer(blogs_obj_list, many=True)
-        return response.Response(blogs.data)
-
-    def post(self, request):
-        input_data = request.data
-        b_obj = serializer.BlogSerializer(data=input_data)
-        if b_obj.is_valid():
-            b_obj.save()
-            return response.Response(b_obj.data, status=status.HTTP_201_CREATED)
-        return response.Response(b_obj.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class BlogGetUpdateView(generics.ListCreateAPIView):
-    serializer_class = serializer.BlogSerializer
-
-    def get_queryset(self):
-        blogs_queryset = models.Blog.objects.filter(id__gt=1)
-        return blogs_queryset
-
-
-class BlogGetUpdateFilterView(generics.ListAPIView):
-    serializer_class = serializer.BlogSerializer
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ["title"]
-
-
-# ============================================================================ #
+from blog.tasks import send_email_to_followers
+from blog.models import Blog
+from blog.serializers import BlogSerializer
+from common.logging_util import log_event
+from config.celery import debug_task
 
 
 def update_blog_title(request):
     blog_id = request.GET.get("id")
-    blog = models.Blog.objects.get(id=blog_id)
+    blog = Blog.objects.get(id=blog_id)
     if request.user.has_perm("blog.update_title"):
         # perform operation
         return HttpResponse("User has permission to update title")
@@ -67,10 +36,95 @@ def blog_view(request):
     return Response(status=200)
 
 
-# ============================================================================ #
+# Cache the result of this function for 10 minutes, it would be unique for each author_id.
+@cached(timeout=60 * 10)
+def get_all_blogs(author_id):
+    print("Fetching blogs from database")
+    blogs = Blog.objects.filter(author_id=author_id)
+    blogs_data = BlogSerializer(blogs, many=True).data
+    return blogs_data
 
 
+def demo():
+    log_event("demo", {"author_id": 1})
+
+
+@api_view(["GET"])
+def get_blogs_by_author(request):
+    author_id = request.GET.get("author_id")
+    blogs = get_all_blogs(author_id)
+    log_event("get_blogs_by_author", {"author_id": author_id})
+    demo()
+    return Response({"blogs": blogs})
+
+
+# Throttling anonymous users.
+class BlogApiView(APIView):
+    throttle_classes = [AnonRateThrottle]
+
+    def get(self, request):
+        content = {"status": "request was permitted"}
+        return Response(content)
+
+
+class Blog2ApiView(APIView):
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = "blog_limit"
+
+    def get(self, request):
+        content = {"status": "request was permitted"}
+        return Response(content)
+
+
+class BlogDetailApiView(APIView):
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = "blog_limit"
+
+    def get(self, request):
+        content = {"status": "request was permitted"}
+        return Response(content)
+
+
+class Blog3ApiView(APIView):
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = "blog_2_limit"
+
+    def get(self, request):
+        content = {"status": "request was permitted"}
+        return Response(content)
+
+
+# Unpaginated view for blogs returning all the blogs in the database.
+@api_view(["GET"])
 def get_blog_without_pagination(request):
     blogs = Blog.objects.all()
     blogs_data = BlogSerializer(blogs, many=True).data
     return Response({"blogs": blogs_data})
+
+
+# Paginated view for blogs returning 10 blogs per page.
+@api_view(["GET"])
+def get_blog_with_pagination(request):
+    page = int(request.GET.get("page", 1))
+    page_size = int(request.GET.get("page_size", 10))
+    offset = (page - 1) * page_size
+    limit = page * page_size
+    blogs = Blog.objects.all()[offset:limit]
+    blogs_data = BlogSerializer(blogs, many=True).data
+    return Response({"blogs": blogs_data})
+
+
+@api_view(["GET"])
+def publish_blog(request):
+    blog_id = request.GET.get("blog_id")
+    author_id = request.GET.get("author_id")
+    print(f"Publishing blog {blog_id}")
+    send_email_to_followers.delay(author_id, blog_id)
+    return Response({"status": "success"})
+
+
+@api_view(["GET"])
+def verify_blog(request):
+    verify_word = request.GET.get("verify_word")
+    debug_task.delay(f"Celery Task verification: {verify_word}")
+    return Response({"status": "success"})
